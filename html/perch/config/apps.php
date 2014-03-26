ffcf369daf31c46e9d3f539126b1dead82c9540b
MISA <letshavemisasoup@gmail.com>
2014-03-26 11:11:50 +0900

    event update

--- html/perch/config/apps.php
+++ html/perch/config/apps.php
@@ -1,4 +1,5 @@
 <?php
     include(PERCH_PATH.'/core/apps/content/runtime.php');
     include(PERCH_PATH.'/addons/apps/perch_members/runtime.php');
+    include(PERCH_PATH.'/addons/apps/perch_events/runtime.php');
 ?>
