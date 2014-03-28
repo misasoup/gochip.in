1ef0a7439fabde007eebcf2ed30e2d9e21d5accb
MISA <letshavemisasoup@gmail.com>
2014-03-28 14:07:46 +0900

    3/28

---
+++ html/members/logout.php
@@ -0,0 +1,5 @@
+<?php 
+	include('../perch/runtime.php');
+	perch_member_log_out(); 
+	PerchUtil::redirect('/members/');
+?>
---
+++ html/members/reset.php
@@ -0,0 +1,71 @@
+<?php include('../perch/runtime.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+	<title>Perch Example Page</title>
+	<?php perch_get_css(); ?>
+</head>
+<body>
+	<header class="layout-header">
+		<div class="wrapper">
+			<div class="company-name">Perch Members App</div>
+			<img src="<?php perch_path('feathers/quill/img/logo.gif'); ?>" alt="Your Logo Here" class="logo"/>
+		</div>
+		<nav class="main-nav">
+			<?php perch_pages_navigation(array(
+					'levels'=>1
+				));
+			?>
+		</nav>
+	</header>
+	
+	<div class="wrapper cols2-nav-right">
+	
+		<div class="primary-content">
+	<?php 
+		if (perch_member_logged_in()) {
+			echo '<p>You are already logged in.</p>';			
+		}else{
+			echo '<h1>Get a new password</h1>';
+			perch_member_form('reset_password.html');
+		}
+
+	?>
+	</div>
+		
+		<nav class="sidebar">
+			<?php
+				if (perch_member_logged_in()) {
+			?>	
+				<ul>
+					<li><a href="profile.php">Edit profile</a></li>
+					<li><a href="logout.php">Log out</a></li>
+				</ul>
+
+			<?php
+				}else{
+					perch_members_login_form();	
+				}
+			?>
+		</nav>
+		
+	</div>
+	
+	<footer class="layout-footer">
+		<div class="wrapper">
+			<ul class="social-links">
+				<li class="twitter"><a href="#" rel="me">Twitter</a></li>
+				<li class="facebook"><a href="#" rel="me">Facebook</a></li>
+				<li class="flickr"><a href="#" rel="me">Flickr</a></li>
+				<li class="linkedin"><a href="#" rel="me">LinkedIn</a></li>
+				<li class="rss"><a href="#">RSS</a></li>
+			</ul>
+			<small>Copyright &copy; 2013</small>
+		</div>
+	</footer>
+
+
+    <?php perch_get_javascript(); ?>
+</body>
+</html>
